24e908d2ae7c5c85067682b00a1bbdb3109ca520
imcybergeek <imcybergeek@gmail.com>
2022-07-01 17:48:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const data = [
+  {
+    img: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    name: "The Avengers (2012)",
+  },
+  {
+    img: "https://media.comicbook.com/2017/10/avengers-2-movie-poster-marvel-cinematic-universe-1038898.jpg",
+    name: "The Avengers: Age of Ultron",
+  },
+  {
+    img: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_FMjpg_UX1000_.jpg",
+    name: "Avengers: Infinity War",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    name: "Avengers: Endgame",
+  },
+  {
+    img: "https://media.comicbook.com/2017/10/iron-man-movie-poster-marvel-cinematic-universe-1038878.jpg",
+    name: "Iron Man",
+  },
+  {
+    img: "https://media.comicbook.com/2017/10/iron-man-2-movie-poster-marvel-cinematic-universe-1038887.jpg",
+    name: "Iron Man 2",
+  },
+  {
+    img: "https://media.comicbook.com/2017/10/iron-man-3-movie-poster-marvel-cinematic-universe-1038894.jpg",
+    name: "Iron Man 3",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((movie) => (
+        <div className="Movie-tile">
+          <img src={movie.img} className="App-logo" alt="logo" />
+          <p>{movie.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
